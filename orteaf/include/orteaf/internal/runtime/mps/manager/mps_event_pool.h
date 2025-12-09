7ea35923789e953873e69aa2184053ea2d695a46
WARE10sai <WARE10sai@icloud.com>
2025-12-09 11:32:08 +0900

    reafctor for manager

--- orteaf/include/orteaf/internal/runtime/mps/manager/mps_event_pool.h
+++ orteaf/include/orteaf/internal/runtime/mps/manager/mps_event_pool.h
@@ -5,35 +5,42 @@
 #include "orteaf/internal/runtime/base/resource_pool.h"
 #include "orteaf/internal/runtime/mps/platform/mps_slow_ops.h"
 #include "orteaf/internal/runtime/mps/platform/wrapper/mps_event.h"
+#include "orteaf/internal/base/handle.h"
+#include "orteaf/internal/base/shared_lease.h"
+#include "orteaf/internal/runtime/base/base_manager.h"
+
+#include <atomic>
 
 namespace orteaf::internal::runtime::mps::manager {
 
+struct EventPoolState {
+    std::atomic<std::size_t> ref_count;
+    ::orteaf::internal::runtime::mps::platform::wrapper::MPSEvent_t event;
+    uint32_t generation;
+    bool alive;
+    bool in_use;
+};
+
 struct EventPoolTraits {
-  using ResourceType =
-      ::orteaf::internal::runtime::mps::platform::wrapper::MPSEvent_t;
+  using StateType = EventPoolState;
   using DeviceType =
       ::orteaf::internal::runtime::mps::platform::wrapper::MPSDevice_t;
   using OpsType = ::orteaf::internal::runtime::mps::platform::MpsSlowOps;
 
   static constexpr const char *Name = "MPS event pool";
 
-  static ResourceType create(OpsType *ops, DeviceType device) {
-    return ops->createEvent(device);
-  }
-  static void destroy(OpsType *ops, ResourceType res) {
-    ops->destroyEvent(res);
-  }
 };
 
+
 class MpsEventPool
-    : public ::orteaf::internal::runtime::base::ResourcePool<MpsEventPool,
+    : public ::orteaf::internal::runtime::base::BaseManager<MpsEventPool,
                                                              EventPoolTraits> {
 public:
-  using Base = ::orteaf::internal::runtime::base::ResourcePool<MpsEventPool,
+  using Base = ::orteaf::internal::runtime::base::BaseManager<MpsEventPool,
                                                                EventPoolTraits>;
   using SlowOps = Base::Ops;
-  using Event = Base::Resource;
-  using EventLease = Base::PoolLease;
+  using EventHandle = ::orteaf::internal::base::EventHandle;
+  using EventLease = ::orteaf::internal::base::SharedLease<EventHandle, ::orteaf::internal::runtime::mps::platform::wrapper::MPSEvent_t, MpsEventPool>;
 
   MpsEventPool() = default;
   MpsEventPool(const MpsEventPool &) = delete;
@@ -42,15 +49,30 @@ public:
   MpsEventPool &operator=(MpsEventPool &&) = default;
   ~MpsEventPool() = default;
 
-  EventLease acquireEvent() { return acquire(); }
+  void initialize(::orteaf::internal::runtime::mps::platform::wrapper::MPSDevice_t device,
+                  SlowOps *slow_ops, std::size_t capacity);
+
+  void shutdown();
+
+  EventLease acquire();
+  EventLease acquire(EventHandle handle);
+
+  void release(EventLease &lease) noexcept;
+
+#if ORTEAF_ENABLE_TEST
+  struct DebugState {
+    bool alive{false};
+    std::uint32_t generation{0};
+    std::size_t growth_chunk_size{0};
+  };
+
+  DebugState debugState(::orteaf::internal::base::EventHandle handle) const;
+#endif
+
+private:
+  void release(EventHandle &handle);
 };
 
 } // namespace orteaf::internal::runtime::mps::manager
-
-namespace orteaf::internal::runtime::base {
-extern template class ResourcePool<
-    ::orteaf::internal::runtime::mps::manager::MpsEventPool,
-    ::orteaf::internal::runtime::mps::manager::EventPoolTraits>;
-}
 
 #endif // ORTEAF_ENABLE_MPS
