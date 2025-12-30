448124458d367e6e1ccdc1320c831ba568fd46e3
WARE10sai <WARE10sai@icloud.com>
2025-12-30 17:50:34 +0900

    feat: add mps fence

---
+++ orteaf/include/orteaf/internal/execution/mps/resource/mps_fence.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#if ORTEAF_ENABLE_MPS
+
+#include <orteaf/internal/base/handle.h>
+#include <orteaf/internal/execution/mps/platform/wrapper/mps_command_buffer.h>
+#include <orteaf/internal/execution/mps/platform/wrapper/mps_fence.h>
+
+namespace orteaf::internal::execution::mps::manager {
+struct FencePayloadPoolTraits;
+}
+
+namespace orteaf::internal::execution::mps::resource {
+
+class MpsFence {
+public:
+  using FenceType =
+      ::orteaf::internal::execution::mps::platform::wrapper::MpsFence_t;
+  using CommandBufferType =
+      ::orteaf::internal::execution::mps::platform::wrapper::MpsCommandBuffer_t;
+  using CommandQueueHandle = ::orteaf::internal::base::CommandQueueHandle;
+
+  MpsFence() = default;
+
+  FenceType fence() const noexcept { return fence_; }
+  CommandBufferType commandBuffer() const noexcept { return command_buffer_; }
+  CommandQueueHandle commandQueueHandle() const noexcept {
+    return command_queue_handle_;
+  }
+
+  bool hasFence() const noexcept { return fence_ != nullptr; }
+  bool hasCommandBuffer() const noexcept { return command_buffer_ != nullptr; }
+
+  bool setFence(FenceType fence) noexcept {
+    if (fence_ != nullptr || command_buffer_ != nullptr) {
+      return false;
+    }
+    fence_ = fence;
+    return true;
+  }
+
+  bool setCommandBuffer(CommandBufferType command_buffer) noexcept {
+    if (command_buffer_ != nullptr) {
+      return false;
+    }
+    command_buffer_ = command_buffer;
+    return true;
+  }
+
+  bool setCommandQueueHandle(CommandQueueHandle handle) noexcept {
+    if (command_buffer_ != nullptr) {
+      return false;
+    }
+    command_queue_handle_ = handle;
+    return true;
+  }
+
+  bool isReady() {
+    if (command_buffer_ == nullptr) {
+      return true;
+    }
+    if (::orteaf::internal::execution::mps::platform::wrapper::isCompleted(
+            command_buffer_)) {
+      command_buffer_ = nullptr;
+      return true;
+    }
+    return false;
+  }
+
+  bool isCompleted() const noexcept { return command_buffer_ == nullptr; }
+
+private:
+  void reset() noexcept {
+    fence_ = nullptr;
+    command_buffer_ = nullptr;
+    command_queue_handle_ = {};
+  }
+
+  friend struct ::orteaf::internal::execution::mps::manager::
+      FencePayloadPoolTraits;
+
+  FenceType fence_{nullptr};
+  CommandBufferType command_buffer_{nullptr};
+  CommandQueueHandle command_queue_handle_{};
+};
+
+} // namespace orteaf::internal::execution::mps::resource
+
+#endif // ORTEAF_ENABLE_MPS
