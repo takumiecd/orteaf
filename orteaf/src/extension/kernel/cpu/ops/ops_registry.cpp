8be27ca3ae74f028d6fe4321bfbb5c14af0dd39b
WARE10sai <WARE10sai@icloud.com>
2026-02-04 10:25:24 +0900

    move directory

--- orteaf/src/extension/kernel/cpu/ops/ops_registry.cpp
+++
@@ -1,6 +0,0 @@
-#include "orteaf/extension/kernel/cpu/ops/print_kernel.h"
-
-#include <orteaf/internal/kernel/registry/kernel_auto_registry.h>
-
-ORTEAF_REGISTER_KERNEL(
-    ::orteaf::extension::kernel::cpu::ops::registerPrintKernelDefault);
